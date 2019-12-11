e5dc1710fe378e3e7ffb97b6993d59f7a506f3a5
svrakata <svrakata@gmail.com>
2019-12-11 18:58:45 +0200

    comparing between mesh and kegg entries and building lists of matched and missmatched entries

--- src/find_tree_branch_value.ts
+++ src/find_tree_branch_value.ts
@@ -1,6 +1,5 @@
 const findTreeBranchValue = (path: string[], map: object) => {
     let value: any = map
-
     path.forEach((prop) => {
         if (value.hasOwnProperty(prop)) {
             value = value[ prop ]
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
-// get all tree numbers and descriptor ui
-
 import fs from "fs"
 import path from "path"
-import generateMeSHTreeMap from "./mesh_tree_map"
+
+import matchKeggToMesh from "./match_kegg_to_mesh"
+import generateMeSHDescUIMap from "./mesh_descUI_map"
+import generateMeSHNameMap from "./mesh_names_map"
+
 
 const meshMapsDIRName = "mesh_maps"
 fs.mkdirSync(path.resolve(__dirname, meshMapsDIRName), { recursive: true })
 
-generateMeSHTreeMap(
-    // MeSH xml to read
-    path.resolve(__dirname, "temp", "document.xml"),
-    // JSON to spit to
-    path.resolve(__dirname, meshMapsDIRName, "mesh_map_test.json"),
-)
+// generateMeSHTreeMap(
+//     // MeSH xml to read
+//     path.resolve(__dirname, "temp", "document.xml"),
+//     // JSON to spit to
+//     path.resolve(__dirname, meshMapsDIRName, "mesh_map_test.json"),
+// )
 
 
+// generateMeSHNameMap(
+//     path.resolve(__dirname, "temp", "desc2020.xml"),
+//     path.resolve(__dirname, meshMapsDIRName, "mesh_names_map.json"),
+// )
+
+// generateMeSHDescUIMap(
+//     path.resolve(__dirname, "temp", "desc2020.xml"),
+//     path.resolve(__dirname, meshMapsDIRName, "mesh_descUI_map.json"),
+// )
+
+const keggList = fs.readFileSync(path.resolve(__dirname, "temp", "kegg_list.csv")).toString()
+
+const keggListSplited = keggList.split("\n")
+
+console.log(matchKeggToMesh(keggListSplited))
