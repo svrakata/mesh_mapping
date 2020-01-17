ed7bcc3fb22224fb3562fdcb1a95180c408bbb43
svrakata <svrakata@gmail.com>
2020-01-17 18:43:42 +0200

    all matches in one file

--- src/match_csv_to_mesh.ts
+++ src/match_csv_to_mesh.ts
@@ -8,7 +8,7 @@ import matchEntryToMeSH from "./match_entry_to_mesh"
 /*
     Takes input folder which is supposed to contain CSV files with proper headers
     Read all the files and match each entry to the MeSH structure.
-    Generates 2 files one with all matched entries plus related terms, the second one is with the missmatched entries.
+    Generates 1 file one with all matched and missmatched entries.
 */
 
 
@@ -27,10 +27,12 @@ const matchCSVToMeSH = (dataFolderPath: string, outputFolderPath: string) => {
         const filePath = path.resolve(__dirname, dataFolderPath, file)
         const fileData = fs.readFileSync(filePath).toString()
         const csvParse = parser(fileData, parserOptions)
-        const matched = []
-        const missMatched = []
+
         const matchedMap = {}
+        const entries = []
+
         let matchedCount = 0
+        let missMatchedCount = 0
 
         csvParse.on("readable", () => {
             let chunk = csvParse.read()
@@ -39,21 +41,20 @@ const matchCSVToMeSH = (dataFolderPath: string, outputFolderPath: string) => {
 
                 if (chunk.hasOwnProperty(csvHeader)) {
                     const entry = matchEntryToMeSH(chunk[ csvHeader ])
+                    entry.value.forEach((entryValue) => {
+                        const { name, matched } = entryValue
 
-                    if (entry.matched) {
-                        entry.value.forEach((entryValue) => {
-                            if (!matchedMap.hasOwnProperty(entryValue.name)) {
-
-                                matchedMap[ entryValue.name ] = 1
-                                matched.push(entryValue)
-                            }
-                        })
-                        matchedCount++
-                    } else {
-                        if (!matchedMap.hasOwnProperty(entry.value[ 0 ].name)) {
-                            missMatched.push(...entry.value)
+                        if (!matchedMap.hasOwnProperty(name)) {
+                            matchedMap[ entryValue.name ] = 1
+                            entries.push(entryValue)
                         }
-                    }
+
+                        if (matched === 1) {
+                            matchedCount++
+                        } else {
+                            missMatchedCount++
+                        }
+                    })
 
                     chunk = csvParse.read()
                 } else {
@@ -66,31 +67,19 @@ const matchCSVToMeSH = (dataFolderPath: string, outputFolderPath: string) => {
         csvParse.on("end", () => {
             console.log(`File "${file}" processed.`)
             console.log(`${matchedCount} entries matched in MeSH.`)
-            console.log(`${missMatched.length} entries not matched in MeSH.\n`)
+            console.log(`${missMatchedCount} entries not matched in MeSH.\n`)
 
-            const fileName = path.parse(file).name
-            const matchedOutputFileName = `${fileName}_matched.csv`
-            const missmatchedOutputFileName = `${fileName}_missmatched.csv`
-            const matchedOutputFilePath = path.resolve(__dirname, outputFolderPath, matchedOutputFileName)
-            const missMatchedOutputFilePath = path.resolve(__dirname, outputFolderPath, missmatchedOutputFileName)
-            const matchedWriteStream = fs.createWriteStream(matchedOutputFilePath)
-            const missMatchedWriteStream = fs.createWriteStream(missMatchedOutputFilePath)
+            const outputfileName = `${path.parse(file).name}.csv`
+            const outputfilePath = path.resolve(__dirname, outputFolderPath, outputfileName)
+            const writeStream = fs.createWriteStream(outputfilePath)
+            const outputCSVHeader = `"meshID","name","terms","matched"\n`
 
-            const outputCSVHeader = `"id","name","terms"`
+            writeStream.write(outputCSVHeader)
 
-            matchedWriteStream.write(`${outputCSVHeader}\n`)
-            missMatchedWriteStream.write(`"name"\n`)
-
-            matched.forEach((entry) => {
-                const { id, name, terms } = entry
-                matchedWriteStream.write(`"${id}","${name}","${terms.join(";")}"\n`)
+            entries.forEach((entry) => {
+                const { id, name, terms, matched } = entry
+                writeStream.write(`"${id}","${name}","${terms.join(";")}","${matched}"\n`)
             })
-
-            missMatched.forEach((entry) => {
-                const { name } = entry
-                missMatchedWriteStream.write(`"${name}"\n`)
-            })
-
         })
 
     })
